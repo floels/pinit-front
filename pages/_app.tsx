2c777c534ec55f3ad5024a809b402b958dfe2059
Florian Ellis <florian.ellis@qonto.com>
2023-03-13 18:14:05 +0100

    Wrap app in <IntlProvider/> from react-intl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// Internationalization: see example at https://github.com/vercel/next.js/blob/canary/examples/with-react-intl/pages/_app.tsx
+type MessageConfig = Record<string, string>;
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ intlMessages: MessageConfig }>) {
+  const { locale, defaultLocale } = useRouter();
+  return (
+    <IntlProvider
+      locale={locale as string}
+      defaultLocale={defaultLocale}
+      messages={pageProps.intlMessages}
+    >
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 }
