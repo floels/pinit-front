1c44d53d3b27f5097b2d2301b590558704f342d0
Florian Ellis <florian.ellis@qonto.com>
2023-03-22 08:18:59 +0100

    Add logo in <HeaderNotLoggedIn />

--- components/Header/HeaderNotLoggedIn.tsx
+++ components/Header/HeaderNotLoggedIn.tsx
@@ -1,4 +1,5 @@
 import { useIntl } from "react-intl";
+import Image from "next/image";
 import styles from "./HeaderNotLoggedIn.module.css";
 
 type HeaderNotLoggedInProps = {
@@ -12,7 +13,10 @@ const HeaderNotLoggedIn = ({
 
   return (
     <div className={styles.container}>
-      <div className={styles.logoContainer}></div>
+      <div className={styles.logoContainer}>
+        <Image src="/images/logo.svg" alt="PinIt logo" width={32} height={32} />
+        <h1 className={styles.logoHeader}>PinIt</h1>
+      </div>
       <button className={styles.signInButton} onClick={handleClickSignInButton}>
         {intl.formatMessage({ id: "SIGN_IN" })}
       </button>
