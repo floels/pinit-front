447cbbb40c4456d936a179d029efeaa9d6d707dc
Florian Ellis <florian.ellis@qonto.com>
2023-04-01 08:38:19 +0200

    Install Cypress and create dummy test for base route

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,15 +1,15 @@
 // see https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
-const nextJest = require('next/jest')
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
   dir: './',
 })
 
 /** @type {import('jest').Config} */
-const customJestConfig = {
+const config = {
   setupFilesAfterEnv: ['./mocks/globalMocks.js'],
   testEnvironment: 'jest-environment-jsdom',
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+export default createJestConfig(config)
