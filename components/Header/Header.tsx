eb393edfbd5a95c6f896b59237d56d7870917465
Florian Ellis <florian.ellis@qonto.com>
2023-04-11 08:19:21 +0200

    Fix initialization of global state

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,17 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import Cookies from "js-cookie";
 import GlobalStateContext from "../../app/globalState";
 import HeaderAuthenticated from "./HeaderAuthenticated";
 import HeaderUnauthenticated from "./HeaderUnauthenticated";
 
 const Header = () => {
-  const { state } = useContext(GlobalStateContext);
+  const { state, dispatch } = useContext(GlobalStateContext);
+
+  useEffect(() => {
+    if (Cookies.get("accessToken")) {
+      dispatch({ type: "SET_IS_AUTHENTICATED", payload: true });
+    }
+  }, [dispatch]);
 
   return state.isAuthenticated ? (
     <HeaderAuthenticated />
