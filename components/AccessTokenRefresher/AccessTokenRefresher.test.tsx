86bcaef7d2fdcc4dc35921158a492c90cc223d3e
Florian Ellis <florian.ellis@qonto.com>
2023-05-19 22:17:30 +0200

    Fix dummy test for <AccessTokenRefresher /> component

--- components/AccessTokenRefresher/AccessTokenRefresher.test.tsx
+++ components/AccessTokenRefresher/AccessTokenRefresher.test.tsx
@@ -1,6 +1,8 @@
 import { render } from "@testing-library/react";
 import AccessTokenRefresher from "./AccessTokenRefresher";
 
+jest.mock("next/navigation", () => require("next-router-mock"));
+
 describe("AccessTokenRefresh", () => {
     it("should render", () => {
         render(<AccessTokenRefresher />);
