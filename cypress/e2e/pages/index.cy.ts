9bd48597a1cd65123bd915fe6016056c60c70772
Florian Ellis <florian.ellis@qonto.com>
2023-05-26 15:20:14 +0200

    Simplify E2E testing of unauthenticated homepage

--- cypress/e2e/pages/index.cy.ts
+++ cypress/e2e/pages/index.cy.ts
@@ -12,25 +12,6 @@ describe("Home page", () => {
     cy.contains(labels.PictureSlider.GET_YOUR_NEXT).should("be.visible");
     cy.contains(labels.PictureSlider.HEADER_FOOD).should("be.visible");
 
-    // Open and close login form
-    cy.contains(labels.Header.LOG_IN).click();
-    cy.contains(labels.Header.LoginForm.WELCOME_TO_PINIT).should("be.visible");
-    cy.get("[data-testid=overlay-modal-close-button]").click();
-    cy.contains(labels.Header.LoginForm.WELCOME_TO_PINIT).should("not.exist");
-
-    // Open login form, switch to signup form and then back to login form
-    cy.contains(labels.Header.LOG_IN).click();
-    cy.contains(labels.Header.LoginForm.NO_ACCOUNT_YET).within(() => {
-      cy.contains(labels.Header.LoginForm.SIGN_UP).click();
-    });
-    cy.contains(labels.Header.SignupForm.FIND_NEW_IDEAS).should("be.visible");
-
-    cy.contains(labels.Header.SignupForm.ALREADY_HAVE_ACCOUNT).within(() => {
-      cy.contains(labels.Header.SignupForm.LOG_IN).click();
-    });
-    cy.contains(labels.Header.LoginForm.NO_ACCOUNT_YET).should("be.visible");
-    cy.get("[data-testid=overlay-modal-close-button]").click();
-
     // Test transitions between first and second fold
     cy.document().trigger("wheel", { deltaY: 1 });
     cy.get("[data-testid=homepage-unauthenticated-content]").should(
