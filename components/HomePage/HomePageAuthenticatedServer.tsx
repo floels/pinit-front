7aa77ecbd122e3aa4d5d372b6b72b079a3c2ced4
Florian Ellis <florian.ellis@qonto.com>
2023-06-07 09:31:38 +0200

    Return dummy pin suggestion in fetchPinSuggestions

--- components/HomePage/HomePageAuthenticatedServer.tsx
+++ components/HomePage/HomePageAuthenticatedServer.tsx
@@ -6,7 +6,13 @@ type HomePageAuthenticatedServerProps = {
 
 const fetchPinSuggestions = async (accessToken: string) => {
     // TODO: implement data fetching
-    return {};
+    return [{
+        id: "0000",
+        title: "This is a pin",
+        imageURL: "https://image.url.com",
+        authorUsername: "johndoe",
+        authorDisplayName: "John DOe",
+    }];
   };
 
 const HomePageAuthenticatedServer = async ({ accessToken }: HomePageAuthenticatedServerProps) => {
