ba62ad3c3afe4cdc39666867c4181d7a040390c6
Florian Ellis <florian.ellis@qonto.com>
2023-07-09 08:18:49 +0200

    Add Playwright test for sign-up flow

---
+++ e2e-tests/signup.spec.ts
@@ -0,0 +1,57 @@
+import { test } from "@playwright/test";
+import { Response, Express } from "express";
+import { Server } from "http";
+import en from "@/messages/en.json";
+import { PORT_MOCK_API_SERVER, checkCookieValue, getExpressApp } from "./utils";
+
+const EMAIL_FIXTURE = "john.doe@example.com";
+const PASSWORD_FIXTURE = "Pa$$w0rd";
+const BIRTHDATE_FIXTURE = "1970-01-01";
+
+let mockAPIApp: Express;
+let mockAPIServer: Server;
+
+const configureAPIResponses = () => {
+  mockAPIApp.post("/api/signup/", (_, response: Response) => {
+    response.json({
+      access_token: "mock_access_token",
+      refresh_token: "mock_refresh_token",
+    });
+  });
+};
+
+const launchMockAPIServer = () => {
+  mockAPIApp = getExpressApp();
+
+  configureAPIResponses();
+
+  mockAPIServer = mockAPIApp.listen(PORT_MOCK_API_SERVER);
+};
+
+test("Should be able to sign up", async ({ page }) => {
+  launchMockAPIServer();
+
+  await page.goto("/");
+
+  await page.click(`text=${en.HomePageUnauthenticated.Header.SIGN_UP}`);
+
+  await page.fill("input[name='email']", EMAIL_FIXTURE);
+  await page.fill("input[name='password']", PASSWORD_FIXTURE);
+  await page.fill("input[name='birthdate']", BIRTHDATE_FIXTURE);
+
+  await page.click(
+    `text=${en.HomePageUnauthenticated.Header.SignupForm.CONTINUE}`
+  );
+
+  // We should land on authenticated homepage
+  await page.waitForSelector(
+    `text=${en.HomePageAuthenticated.Header.NAV_ITEM_HOME}`
+  );
+
+  // Check presence of authentication cookies
+  await checkCookieValue(page, "accessToken", "mock_access_token");
+  await checkCookieValue(page, "refreshToken", "mock_refresh_token");
+
+  // Close mock API server
+  mockAPIServer.close();
+});
