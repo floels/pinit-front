944a5f85449f0919c637799d9f8755b7b690b0a4
Florian Ellis <florian.ellis@qonto.com>
2023-07-29 07:16:58 +0200

    Unauthenticated homepage: add picture elements in fourth fold

--- components/HomePageUnauthenticated/FourthFold.tsx
+++ components/HomePageUnauthenticated/FourthFold.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./FourthFold.module.css";
 import TextAndLink from "./TextAndLink";
 
@@ -19,7 +20,39 @@ const FourthFold = ({ labels }: FourthFoldProps) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.picturesArea}></div>
+      <div className={styles.picturesArea}>
+        <Image
+          src="https://s.pinimg.com/webapp/shop-bd0c8a04.png"
+          alt={labels.LIP_SHADE_PICTURE_ALT}
+          fill
+          sizes="50vw"
+          className={styles.coverPicture}
+        />
+        <div className={styles.picturesOverlay}>
+          <Image
+            src="https://s.pinimg.com/webapp/creator-pin-img-491ebb56.png"
+            alt={labels.EYE_SHADE_PICTURE_ALT}
+            width={216}
+            height={384}
+            className={styles.secondaryPicture}
+          />
+          <Image
+            src="https://s.pinimg.com/webapp/creator-avatar-d7a05622.png"
+            alt={labels.CREATOR_THUMBNAIL_PICTURE}
+            width={96}
+            height={96}
+            className={styles.thumbnailPicture}
+          />
+          <div className={styles.secondaryPictureText}>
+            <span className={styles.secondaryPictureTextTitle}>
+              {labels.SECONDARY_PICTURE_TEXT_TITLE}
+            </span>
+            <span className={styles.secondaryPictureTextSubtitle}>
+              {labels.SECONDARY_PICTURE_TEXT_SUBTITLE}
+            </span>
+          </div>
+        </div>
+      </div>
       <div className={styles.textArea}>
         <TextAndLink
           labels={textAndLinkLabels}
