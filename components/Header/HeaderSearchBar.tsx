ff9e4424cdfa04ae7b814ccfe03230a2679a52a5
Florian Ellis <florian.ellis@qonto.com>
2023-10-05 07:13:47 +0200

    Add autocomplete suggestions dropdown in header search bar

--- components/Header/HeaderSearchBar.tsx
+++ components/Header/HeaderSearchBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import styles from "./HeaderSearchBar.module.css";
@@ -7,11 +7,14 @@ type HeaderSearchBarPros = {
   labels: { [key: string]: string };
 };
 
+const DEBOUNCE_DURATION_MS = 300;
+
 const HeaderSearchBar = ({ labels }: HeaderSearchBarPros) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const [isInputFocused, setIsInputFocused] = useState(false);
   const [inputValue, setInputValue] = useState("");
+  const [autocompleteSuggestions, setAutocompleteSuggestions] = useState([]);
 
   const handleFocusInput = () => {
     setIsInputFocused(true);
@@ -42,6 +45,45 @@ const HeaderSearchBar = ({ labels }: HeaderSearchBarPros) => {
     };
   }, []);
 
+  const fetchAutocompleteSuggestions = useCallback(async () => {
+    let response;
+
+    try {
+      response = await fetch(
+        `/api/pins/search/autocomplete?search=${inputValue}`,
+        { method: "GET" },
+      );
+    } catch (error) {
+      // Fail silently
+      setAutocompleteSuggestions([]);
+      return;
+    }
+
+    if (!response.ok) {
+      // Fail silently
+      setAutocompleteSuggestions([]);
+      return;
+    }
+
+    const responseData = await response.json();
+
+    setAutocompleteSuggestions(responseData.results);
+  }, [inputValue]);
+
+  useEffect(() => {
+    const debounceTimeout = setTimeout(() => {
+      if (inputValue) {
+        fetchAutocompleteSuggestions();
+      } else {
+        setAutocompleteSuggestions([]);
+      }
+    }, DEBOUNCE_DURATION_MS);
+
+    return () => {
+      clearTimeout(debounceTimeout);
+    };
+  }, [inputValue, fetchAutocompleteSuggestions]);
+
   return (
     <div
       className={`${styles.container}
@@ -51,7 +93,7 @@ const HeaderSearchBar = ({ labels }: HeaderSearchBarPros) => {
       {!isInputFocused && (
         <FontAwesomeIcon
           icon={faSearch}
-          className={styles.searchIcon}
+          className={styles.inputSearchIcon}
           data-testid="search-bar-icon"
         />
       )}
@@ -69,6 +111,19 @@ const HeaderSearchBar = ({ labels }: HeaderSearchBarPros) => {
         onBlur={handleBlurSearchBarInput}
         data-testid="search-bar-input"
       />
+      {isInputFocused && autocompleteSuggestions.length > 0 && (
+        <ul className={styles.autocompleteSuggestionsList}>
+          {autocompleteSuggestions.map((suggestion, index) => (
+            <li key={index} className={styles.autocompleteSuggestion}>
+              <FontAwesomeIcon
+                icon={faSearch}
+                className={styles.autocompleteSuggestionSearchIcon}
+              />
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
