a488f7e6d705d7f5a64e162af3b79274870f27ca
Florian Ellis <florian.ellis@qonto.com>
2023-10-06 21:08:28 +0200

    Add loading state for /search/pins route

---
+++ app/[locale]/search/pins/loading.tsx
@@ -0,0 +1,7 @@
+import HomePageContentLoadingState from "@/components/HomePage/HomePageContentLoadingState";
+
+const Loading = () => {
+  return <HomePageContentLoadingState />;
+};
+
+export default Loading;
