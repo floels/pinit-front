175fae7de539f33a5bc83b0ea78667b1f2e76621
Florian Ellis <florian.ellis@qonto.com>
2023-10-16 07:28:49 +0200

    Add links to /pin route in <PinThumbnail />

--- components/PinsBoard/PinThumbnail.tsx
+++ components/PinsBoard/PinThumbnail.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Link from "next/link";
 import styles from "./PinThumbnail.module.css";
 
 export type PinThumbnailType = {
@@ -19,17 +20,21 @@ const PinThumbnail = ({ pinThumbnail, labels }: PinThumbnailProps) => {
   return (
     <div className={styles.container} data-testid="pin-thumbnail">
       {/* We don't use Next's Image component because we don't know the image's display height in advance. */}
-      <img
-        alt={
-          pinThumbnail.title
-            ? pinThumbnail.title
-            : `${labels.PIN_BY} ${pinThumbnail.authorDisplayName}`
-        }
-        src={pinThumbnail.imageURL}
-        className={styles.image}
-      />
+      <Link href={`/pin/${pinThumbnail.id}`}>
+        <img
+          alt={
+            pinThumbnail.title
+              ? pinThumbnail.title
+              : `${labels.PIN_BY} ${pinThumbnail.authorDisplayName}`
+          }
+          src={pinThumbnail.imageURL}
+          className={styles.image}
+        />
+      </Link>
       {pinThumbnail.title && (
-        <div className={styles.title}>{pinThumbnail.title}</div>
+        <Link href={`/pin/${pinThumbnail.id}`} className={styles.title}>
+          {pinThumbnail.title}
+        </Link>
       )}
     </div>
   );
