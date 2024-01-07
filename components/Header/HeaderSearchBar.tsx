646b112d395f15dbf83a25757acd0d04e708ada1
Florian Ellis <florian.ellis@qonto.com>
2024-01-07 17:55:39 +0100

    Split <HeaderSearchBar /> into two components

--- components/Header/HeaderSearchBar.tsx
+++ components/Header/HeaderSearchBar.tsx
@@ -1,93 +1,44 @@
-import { useEffect, useState, useRef, useCallback } from "react";
-import debounce from "lodash/debounce";
+import { useEffect, useRef, useCallback } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faSearch } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useTranslations } from "next-intl";
 import styles from "./HeaderSearchBar.module.css";
-import { API_ROUTE_PINS_SEARCH_AUTOCOMPLETE } from "@/lib/constants";
 
-export const AUTOCOMPLETE_DEBOUNCE_TIME_MS = 300;
-
-const getRefinedSuggestions = (
-  searchTerm: string,
-  suggestionsfromAPI: string[],
-) => {
-  const MAX_SUGGESTIONS = 12;
-
-  const isSearchTermIncludedInSuggestions =
-    suggestionsfromAPI.includes(searchTerm);
-
-  if (isSearchTermIncludedInSuggestions) {
-    // NB: normally the API returns 12 suggestions at most
-    // so this `slice` is just for precaution.
-    return suggestionsfromAPI.slice(0, MAX_SUGGESTIONS);
-  }
-
-  // If search term is not present, add searchTerm as the first suggestion
-  // (and drop the last suggestion received from the API):
-  const remainingSuggestions = suggestionsfromAPI.slice(0, MAX_SUGGESTIONS - 1);
-  return [searchTerm, ...remainingSuggestions];
+type HeaderSearchBarProps = {
+  inputValue: string;
+  isInputFocused: boolean;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onInputFocus: () => void;
+  onInputBlur: () => void;
+  onClickClearIcon: () => void;
+  onPressEscape: () => void;
+  autocompleteSuggestions: string[];
+  getSuggestionLinkClickHandler: (suggestion: string) => () => void;
 };
 
-const HeaderSearchBar = () => {
+const HeaderSearchBar = ({
+  inputValue,
+  isInputFocused,
+  onInputChange,
+  onInputFocus,
+  onInputBlur,
+  onClickClearIcon,
+  onPressEscape,
+  autocompleteSuggestions,
+  getSuggestionLinkClickHandler,
+}: HeaderSearchBarProps) => {
   const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   const t = useTranslations("HeaderAuthenticated");
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const [isInputFocused, setIsInputFocused] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const [autocompleteSuggestions, setAutocompleteSuggestions] = useState<
-    string[]
-  >([]);
-
-  const handleFocusInput = () => {
-    setIsInputFocused(true);
-  };
-
-  const handleBlurInput = () => {
-    setIsInputFocused(false);
-  };
-
-  // We need this additional handler when the user clicks a suggestion link,
-  // in order to circumvent a race condition. Without this event handler, when the user
-  // clicks on a suggestion <Link /> (see below), this `handleBlurInput` handler
-  // is called. This will immediately set isInputFocused to false and unmount the <Link />,
-  // and the transition to the target route will not take place.
-  const getSuggestionLinkClickHandler = (suggestion: string) => {
-    return () => {
-      setInputValue(suggestion); // Theoretically this shouldn't be needed since the input's value
-      // is automatically updated based on the route, but updating it here will give a better impression
-      // of reactivity.
-
-      router.push(`/search/pins?q=${suggestion}`);
-    };
-  };
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
-  // Initialize the input value to the search param if present:
-  useEffect(() => {
-    if (pathname === "/search/pins") {
-      const searchTerm = searchParams.get("q");
-
-      if (searchTerm) {
-        setInputValue(searchTerm);
-      }
-    }
-  }, [pathname, searchParams]);
-
   const handleKeyDown = useCallback(
     (event: KeyboardEvent) => {
       if (event.key === "Escape") {
-        setInputValue("");
+        onPressEscape();
         if (inputRef.current) {
           inputRef.current.blur();
         }
@@ -105,61 +56,6 @@ const HeaderSearchBar = () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
   }, [handleKeyDown]);
-
-  const fetchAutocompleteSuggestions = useCallback(
-    async ({ searchTerm }: { searchTerm: string }) => {
-      let response, responseData;
-
-      try {
-        response = await fetch(
-          `${API_ROUTE_PINS_SEARCH_AUTOCOMPLETE}?search=${searchTerm}`,
-        );
-
-        responseData = await response.json();
-      } catch (error) {
-        // Fail silently
-        setAutocompleteSuggestions([]);
-        return;
-      }
-
-      if (!response.ok) {
-        // Fail silently
-        setAutocompleteSuggestions([]);
-        return;
-      }
-
-      const refinedSuggestions = getRefinedSuggestions(
-        searchTerm,
-        responseData.results,
-      );
-
-      setAutocompleteSuggestions(refinedSuggestions);
-    },
-    [setAutocompleteSuggestions],
-  );
-
-  useEffect(() => {
-    if (!inputValue) {
-      setAutocompleteSuggestions([]);
-      return;
-    }
-
-    const debouncedFetchAutoCompleteSuggestions = debounce(
-      fetchAutocompleteSuggestions,
-      AUTOCOMPLETE_DEBOUNCE_TIME_MS,
-    );
-
-    debouncedFetchAutoCompleteSuggestions({ searchTerm: inputValue });
-
-    return () => {
-      debouncedFetchAutoCompleteSuggestions.cancel();
-    };
-  }, [inputValue, fetchAutocompleteSuggestions]);
-
-  const handleClickClearIcon = () => {
-    setInputValue("");
-    setIsInputFocused(false);
-  };
 
   return (
     <div
@@ -183,9 +79,9 @@ const HeaderSearchBar = () => {
         autoCorrect="off"
         className={styles.searchInput}
         placeholder={t("PLACEHOLDER_SEARCH")}
-        onChange={handleInputChange}
-        onFocus={handleFocusInput}
-        onBlur={handleBlurInput}
+        onChange={onInputChange}
+        onFocus={onInputFocus}
+        onBlur={onInputBlur}
         data-testid="search-bar-input"
       />
       {isInputFocused && (
@@ -194,7 +90,7 @@ const HeaderSearchBar = () => {
         resulting in ``handleClickClearIcon` not being called. For some reason this doesn't happen with `onMouseDown`.*/
         <div
           className={styles.clearIconContainer}
-          onMouseDown={handleClickClearIcon}
+          onMouseDown={onClickClearIcon}
           data-testid="clear-icon"
         >
           <FontAwesomeIcon icon={faCircleXmark} size="lg" />
---
+++ components/Header/HeaderSearchBarContainer.tsx
@@ -0,0 +1,158 @@
+import { useEffect, useState, useRef, useCallback } from "react";
+import debounce from "lodash/debounce";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faSearch } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { API_ROUTE_PINS_SEARCH_AUTOCOMPLETE } from "@/lib/constants";
+import HeaderSearchBar from "./HeaderSearchBar";
+
+export const AUTOCOMPLETE_DEBOUNCE_TIME_MS = 300;
+
+const getRefinedSuggestions = (
+  searchTerm: string,
+  suggestionsfromAPI: string[],
+) => {
+  const MAX_SUGGESTIONS = 12;
+
+  const isSearchTermIncludedInSuggestions =
+    suggestionsfromAPI.includes(searchTerm);
+
+  if (isSearchTermIncludedInSuggestions) {
+    // NB: normally the API returns 12 suggestions at most
+    // so this `slice` is just for precaution.
+    return suggestionsfromAPI.slice(0, MAX_SUGGESTIONS);
+  }
+
+  // If search term is not present, add searchTerm as the first suggestion
+  // (and drop the last suggestion received from the API):
+  const remainingSuggestions = suggestionsfromAPI.slice(0, MAX_SUGGESTIONS - 1);
+  return [searchTerm, ...remainingSuggestions];
+};
+
+const HeaderSearchBarContainer = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isInputFocused, setIsInputFocused] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [autocompleteSuggestions, setAutocompleteSuggestions] = useState<
+    string[]
+  >([]);
+
+  const handleInputFocus = () => {
+    setIsInputFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsInputFocused(false);
+  };
+
+  // We need this additional handler when the user clicks a suggestion link,
+  // in order to circumvent a race condition. Without this event handler, when the user
+  // clicks on a suggestion <Link /> (see below), this `handleBlurInput` handler
+  // is called. This will immediately set isInputFocused to false and unmount the <Link />,
+  // and the transition to the target route will not take place.
+  const getSuggestionLinkClickHandler = (suggestion: string) => {
+    return () => {
+      setInputValue(suggestion); // Theoretically this shouldn't be needed since the input's value
+      // is automatically updated based on the route, but updating it here will give a better impression
+      // of reactivity.
+
+      router.push(`/search/pins?q=${suggestion}`);
+    };
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handlePressEscape = () => {
+    setInputValue("");
+  };
+
+  // Initialize the input value to the search param if present:
+  useEffect(() => {
+    if (pathname === "/search/pins") {
+      const searchTerm = searchParams.get("q");
+
+      if (searchTerm) {
+        setInputValue(searchTerm);
+      }
+    }
+  }, [pathname, searchParams]);
+
+  const fetchAutocompleteSuggestions = useCallback(
+    async ({ searchTerm }: { searchTerm: string }) => {
+      let response, responseData;
+
+      try {
+        response = await fetch(
+          `${API_ROUTE_PINS_SEARCH_AUTOCOMPLETE}?search=${searchTerm}`,
+        );
+
+        responseData = await response.json();
+      } catch (error) {
+        // Fail silently
+        setAutocompleteSuggestions([]);
+        return;
+      }
+
+      if (!response.ok) {
+        // Fail silently
+        setAutocompleteSuggestions([]);
+        return;
+      }
+
+      const refinedSuggestions = getRefinedSuggestions(
+        searchTerm,
+        responseData.results,
+      );
+
+      setAutocompleteSuggestions(refinedSuggestions);
+    },
+    [setAutocompleteSuggestions],
+  );
+
+  useEffect(() => {
+    if (!inputValue) {
+      setAutocompleteSuggestions([]);
+      return;
+    }
+
+    const debouncedFetchAutoCompleteSuggestions = debounce(
+      fetchAutocompleteSuggestions,
+      AUTOCOMPLETE_DEBOUNCE_TIME_MS,
+    );
+
+    debouncedFetchAutoCompleteSuggestions({ searchTerm: inputValue });
+
+    return () => {
+      debouncedFetchAutoCompleteSuggestions.cancel();
+    };
+  }, [inputValue, fetchAutocompleteSuggestions]);
+
+  const handleClickClearIcon = () => {
+    setInputValue("");
+    setIsInputFocused(false);
+  };
+
+  return (
+    <HeaderSearchBar
+      inputValue={inputValue}
+      isInputFocused={isInputFocused}
+      onInputChange={handleInputChange}
+      onInputFocus={handleInputFocus}
+      onInputBlur={handleInputBlur}
+      onClickClearIcon={handleClickClearIcon}
+      onPressEscape={handlePressEscape}
+      autocompleteSuggestions={autocompleteSuggestions}
+      getSuggestionLinkClickHandler={getSuggestionLinkClickHandler}
+    />
+  );
+};
+
+export default HeaderSearchBarContainer;
