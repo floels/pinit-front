f7bcca376285843ad86f8cca55b6892776a74f32
Florian Ellis <florian.ellis@qonto.com>
2024-02-29 06:23:46 +0100

    Introduce 'headerSearchBarContext' context

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -24,6 +24,7 @@ import { ACCESS_TOKEN_COOKIE_KEY } from "@/lib/constants";
 import QueryClientProvider from "@/components/QueryClientProvider/QueryClientProvider";
 import { LogOutContextProvider } from "@/contexts/logOutContext";
 import { AccountContextProvider } from "@/contexts/accountContext";
+import { HeaderSearchBarContextProvider } from "@/contexts/headerSearchBarContext";
 
 type Props = {
   children: React.ReactNode;
@@ -61,13 +62,15 @@ const Layout = ({ children, params: { locale } }: Props) => {
           <QueryClientProvider>
             <LogOutContextProvider>
               <AccountContextProvider>
-                {isAuthenticated && <AuthenticatedSetupBuilder />}
-                {isAuthenticated ? (
-                  <HeaderAuthenticatedContainer />
-                ) : (
-                  <HeaderUnauthenticated />
-                )}
-                {children}
+                <HeaderSearchBarContextProvider>
+                  {isAuthenticated && <AuthenticatedSetupBuilder />}
+                  {isAuthenticated ? (
+                    <HeaderAuthenticatedContainer />
+                  ) : (
+                    <HeaderUnauthenticated />
+                  )}
+                  {children}
+                </HeaderSearchBarContextProvider>
               </AccountContextProvider>
             </LogOutContextProvider>
           </QueryClientProvider>
---
+++ contexts/headerSearchBarContext.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useReducer,
+  Dispatch,
+  ReactNode,
+  useEffect,
+} from "react";
+import { useSearchParams, usePathname } from "next/navigation";
+import { useLocale } from "next-intl";
+
+type State = {
+  inputValue: string;
+  isInputFocused: boolean;
+};
+
+type Action =
+  | { type: "SET_INPUT_VALUE"; payload: string }
+  | { type: "FOCUS_INPUT" }
+  | { type: "BLUR_INPUT" };
+
+type HeaderSearchBarContextType = {
+  state: State;
+  dispatch: Dispatch<Action>;
+};
+
+const initialState = {
+  inputValue: "",
+  isInputFocused: false,
+};
+
+const HeaderSearchBarContext = createContext<
+  HeaderSearchBarContextType | undefined
+>(undefined);
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "SET_INPUT_VALUE":
+      return { ...state, inputValue: action.payload };
+    case "FOCUS_INPUT":
+      return { ...state, isInputFocused: true };
+    case "BLUR_INPUT":
+      return { ...state, isInputFocused: false };
+    default:
+      return state;
+  }
+};
+
+export const HeaderSearchBarContextProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const pathname = usePathname();
+  const locale = useLocale();
+  const searchParams = useSearchParams();
+
+  // Initialize the input value to the search param if present:
+  useEffect(() => {
+    if (pathname === `/${locale}/search/pins`) {
+      const searchTerm = searchParams.get("q");
+      if (searchTerm) {
+        dispatch({ type: "SET_INPUT_VALUE", payload: searchTerm });
+      }
+    }
+  }, [pathname, searchParams, locale]);
+
+  return (
+    <HeaderSearchBarContext.Provider value={{ state, dispatch }}>
+      {children}
+    </HeaderSearchBarContext.Provider>
+  );
+};
+
+export const useHeaderSearchBarContext = () => {
+  const context = useContext(HeaderSearchBarContext);
+
+  if (context === undefined) {
+    throw new Error(
+      "useHeaderSearchBarContext must be used within a HeaderSearchBarProvider",
+    );
+  }
+
+  return context;
+};
