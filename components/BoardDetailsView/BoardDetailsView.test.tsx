5bf51088c5f11e75ba50f16798e8b2e9e6d0ee7e
Florian Ellis <florian.ellis@qonto.com>
2024-03-25 18:53:15 +0100

    Add basic unit test for <BoardDetailsView /> component

---
+++ components/BoardDetailsView/BoardDetailsView.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import BoardDetailsView from "./BoardDetailsView";
+import { MOCK_API_RESPONSES_SERIALIZED } from "@/lib/testing-utils/mockAPIResponses";
+import { API_ENDPOINT_BOARD_DETAILS } from "@/lib/constants";
+import { checkNextImageSrc } from "@/lib/testing-utils/misc";
+
+const board = MOCK_API_RESPONSES_SERIALIZED[API_ENDPOINT_BOARD_DETAILS];
+
+const renderComponent = () => {
+  render(<BoardDetailsView board={board} />);
+};
+
+it("renders author's profile picture", () => {
+  renderComponent();
+
+  const authorProfilePicture = screen.getByTestId(
+    "board-author-profile-picture",
+  );
+
+  checkNextImageSrc(authorProfilePicture, board.author.profilePictureURL);
+});
