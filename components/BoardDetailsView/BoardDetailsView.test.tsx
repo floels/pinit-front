c0c0ff00bcc55cf1e496010da963a16d46b86946
Florian Ellis <florian.ellis@qonto.com>
2024-03-29 08:42:39 +0100

    Improve test for <BoardDetailsView /> component

--- components/BoardDetailsView/BoardDetailsView.test.tsx
+++ components/BoardDetailsView/BoardDetailsView.test.tsx
@@ -3,6 +3,7 @@ import BoardDetailsView from "./BoardDetailsView";
 import { MOCK_API_RESPONSES_SERIALIZED } from "@/lib/testing-utils/mockAPIResponses";
 import { API_ENDPOINT_BOARD_DETAILS } from "@/lib/constants";
 import { checkNextImageSrc } from "@/lib/testing-utils/misc";
+import en from "@/messages/en.json";
 
 const board = MOCK_API_RESPONSES_SERIALIZED[API_ENDPOINT_BOARD_DETAILS];
 
@@ -20,4 +21,10 @@ it("renders board details", () => {
   );
 
   checkNextImageSrc(authorProfilePicture, board.author.profilePictureURL);
+
+  screen.getByText(`${board.pins.length} ${en.BoardDetails.PINS}`);
+
+  const pinThumbnails = screen.getAllByTestId("pin-thumbnail");
+
+  expect(pinThumbnails).toHaveLength(board.pins.length);
 });
